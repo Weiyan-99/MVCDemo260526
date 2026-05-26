78273752585a584d976dcebee0d353a3edbe5969
Weiyan-99 <g2301149040@gmail.com>
2026-05-27 00:03:20 +0800

    新增專案2 新增user vm、controller

---
+++ WebApplication2/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models.EfModels;
+using WebApplication2.ViewModels;
+
+namespace WebApplication2.Controllers
+{
+	public class UsersController : Controller
+	{
+		private readonly ISpanDemoContext _context;
+
+		public UsersController(ISpanDemoContext context)
+		{
+			_context = context;
+		}
+		public IActionResult Index()
+		{
+			var users = _context.Users
+				.Select(u => new UserItemViewModel
+				{
+					Id = u.Id,
+					UserName = u.UserName
+				})
+				.ToList();
+
+
+			return View(users);
+		}
+
+		public IActionResult Create() {
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Create(UserCreateViewModel vm) {
+			if (ModelState.IsValid)
+			{
+				var user = new User() { UserName = vm.UserName };
+				_context.Add(user);
+				_context.SaveChangesAsync();
+
+				//TempData["msg"]="紀錄已新增";
+
+				return RedirectToAction("Index");
+			}
+			return View(vm);
+		}
+	}
+}
---
+++ WebApplication2/ViewModels/UserViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.ViewModels
+{
+	public class UserItemViewModel
+	{
+		public int Id { get; set; }
+
+		[DisplayName("使用者名稱")]
+		public string UserName { get; set; }
+	}
+	public class UserCreateViewModel
+	{
+		public int Id { get; set; }
+
+		[DisplayName("使用者名稱")]
+		[Required(ErrorMessage = "{0}必填")]
+		public string UserName { get; set; }
+	}
+	public class UserUpdateViewModel
+	{
+		public int Id { get; set; }
+
+		[DisplayName("使用者名稱")]
+		[Required(ErrorMessage = "請輸入使用者名稱")]
+		public string UserName { get; set; }
+	}
+}
