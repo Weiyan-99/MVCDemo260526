15ed69c0cfbd3a9ed6821e4681568e343a44b9f0
Weiyan-99 <g2301149040@gmail.com>
2026-05-26 22:47:56 +0800

    常見問答的增刪查改

--- WebApplication1/Controllers/ProductsController.cs
+++ WebApplication1/Controllers/ProductsController.cs
@@ -1,12 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models.EfModels;
 
 namespace WebApplication1.Controllers
 {
 	public class ProductsController : Controller
 	{
+		private readonly dbFirstAppContext _context;
+
+		public ProductsController(dbFirstAppContext context)
+		{
+			_context = context;
+		}
+
+		//Get: Products
+		[HttpGet]
 		public IActionResult Index()
 		{
 			return View();
 		}
+
+		//GET: Products/Create
+		//[HttpGet]
+
+		//// Post: Products/Create
+		//[HttpPost]
+		//[ValidateAntiForgeryToken]
+
+		////GET: Products/Edit/5
+		//[HttpGet]
+
+		//// Post: Products/Edit
+		//[HttpPost]
+		//[ValidateAntiForgeryToken]
+
+
 	}
 }
